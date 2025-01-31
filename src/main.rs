582d869728bd1ebd5a24cf591c0ded472082865a
Mantarain <swimmingmantarain@gmail.com>
2025-01-31 14:11:13 +0100

    2 pp mode

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,8 @@ async fn main() {
 
     let mut score = 0;
     let mut bot_score = 0;
+    let mut bot_mode = false;
+    let mut button_text: &str;
 
     loop {
         // Set the background color
@@ -48,62 +50,73 @@ async fn main() {
             GREEN,
         );
 
-        // Customize button style with green border
-        let button_style = root_ui()
-            .style_builder()
-            .background(
-                Image::from_file_with_format(
-                    include_bytes!("wasm-pong_button_background.png"),
-                     None
-                    ).unwrap())
-            .background_hovered(
-                Image::from_file_with_format(
-                    include_bytes!("wasm-pong_button_bg_clicked.png"),
-                     None
-                ).unwrap()
-            )
-            .text_color(Color::from_rgba(0, 255, 0, 255))
-            .text_color_hovered(Color::from_rgba(0, 255, 0, 255))
-            .text_color_clicked(Color::from_rgba(0, 255, 0, 255))
-            .font_size(20)
-            .build();
-
-        let button_skin = Skin {
-            button_style: button_style,
-            ..root_ui().default_skin()
-        };
-
-        // Mode toggle button
-        root_ui().push_skin(&button_skin);
-        if root_ui().button(vec2(screen_width() - 100.0, 10.0), "Mode") {
-            println!("oiadfjoaij");
-        };
-        root_ui().pop_skin();
-
         // Check for input
         for touch in touches() {
             let pos = touch.position;
-            player_y = pos.y - PADDLE_H / 2.0;
-            // Check for cheating
-            if player_y < 0. {
-                player_y = 0.;
-            }
-            if player_y > screen_height() - PADDLE_H {
-                player_y = screen_height() - PADDLE_H;
-            }
-        }
-        
-        if is_mouse_button_down(MouseButton::Left) {
-            player_y = mouse_position().1 - PADDLE_H / 2.0;
-            // Check for cheating
-            if player_y < 0. {
-                player_y = 0.;
-            }
-            if player_y > screen_height() - PADDLE_H {
-                player_y = screen_height() - PADDLE_H;
+            if bot_mode {
+                player_y = pos.y - PADDLE_H / 2.0;
+                // Check for cheating
+                if player_y < 0. {
+                    player_y = 0.;
+                }
+                if player_y > screen_height() - PADDLE_H {
+                    player_y = screen_height() - PADDLE_H;
+                }
+            } else {
+                if pos.x < screen_width() / 2.0 {
+                    player_y = pos.y - PADDLE_H / 2.0;
+                    // Check for cheating
+                    if player_y < 0. {
+                        player_y = 0.;
+                    }
+                    if player_y > screen_height() - PADDLE_H {
+                        player_y = screen_height() - PADDLE_H;
+                    }
+                } else {
+                    bot_y = pos.y - PADDLE_H / 2.0;
+                    // Check for cheating
+                    if bot_y < 0. {
+                        bot_y = 0.;
+                    }
+                    if bot_y > screen_height() - PADDLE_H {
+                        bot_y = screen_height() - PADDLE_H;
+                    }
+                }
             }
         }
 
+        if is_mouse_button_down(MouseButton::Left) {
+            if bot_mode {
+                player_y = mouse_position().1 - PADDLE_H / 2.0;
+                // Check for cheating
+                if player_y < 0. {
+                    player_y = 0.;
+                }
+                if player_y > screen_height() - PADDLE_H {
+                    player_y = screen_height() - PADDLE_H;
+                }
+            } else {
+                if mouse_position().0 < screen_width() / 2.0 {
+                    player_y = mouse_position().1 - PADDLE_H / 2.0;
+                    // Check for cheating
+                    if player_y < 0. {
+                        player_y = 0.;
+                    }
+                    if player_y > screen_height() - PADDLE_H {
+                        player_y = screen_height() - PADDLE_H;
+                    }
+                } else {
+                    bot_y = mouse_position().1 - PADDLE_H / 2.0;
+                    // Check for cheating
+                    if bot_y < 0. {
+                        bot_y = 0.;
+                    }
+                    if bot_y > screen_height() - PADDLE_H {
+                        bot_y = screen_height() - PADDLE_H;
+                    }
+                }
+            }
+        }
 
         if is_key_down(KeyCode::Up) {
             player_y -= PADDLE_SPEED * delta;
@@ -118,27 +131,40 @@ async fn main() {
             }
         }
 
-        // Update Ball position
+        if !bot_mode {
+            if is_key_down(KeyCode::W) {
+                bot_y -= PADDLE_SPEED * delta;
+                // Check for cheating
+                if bot_y < 0. {
+                    bot_y = 0.;
+                }
+            } else if is_key_down(KeyCode::S) {
+                bot_y += PADDLE_SPEED * delta;
+                if bot_y > screen_height() - PADDLE_H {
+                    bot_y = screen_height() - PADDLE_H;
+                }
+            }
+        }
 
+        // Update Ball position
         ball_x += ball_speed_x as f32 * delta;
         ball_y += ball_speed_y as f32 * delta;
 
         // Update bot paddle position
+        if bot_mode {
+            bot_y = ball_y + BALL_SIZE / 2.0 - PADDLE_H / 2.0;
+            // Check if the bot is cheating
+            if bot_y < 0. {
+                bot_y = 0.;
+            }
 
-        bot_y = ball_y + BALL_SIZE / 2.0 - PADDLE_H / 2.0;
-
-        // Check if the bot is cheating
-
-        if bot_y < 0. {
-            bot_y = 0.;
+            if bot_y > screen_height() - PADDLE_H {
+                bot_y = screen_height() - PADDLE_H;
+            }
         }
 
-        if bot_y > screen_height() - PADDLE_H {
-            bot_y = screen_height() - PADDLE_H;
-        }
-
+        
         // Check for ball collision with walls (uwu)
-
         if ball_x > screen_width() - BALL_SIZE {
             score += 1;
             ball_x = screen_width() / 2.0 - BALL_SIZE / 2.0;
@@ -162,7 +188,6 @@ async fn main() {
         }
 
         // Check for bang bang with player
-
         if check_paddle_col_player(ball_x, ball_y, player_x, player_y) {
             ball_speed_x = {
                 if ball_speed_x > 0 {
@@ -174,7 +199,6 @@ async fn main() {
         }
 
         // Check for bang bang with bot
-
         if check_paddle_col_bot(ball_x, ball_y, bot_x, bot_y) {
             ball_speed_x = {
                 if ball_speed_x > 0 {
@@ -185,8 +209,41 @@ async fn main() {
             };
         }
 
-        // Draw the score
+        // Customize button style with green border
+        let button_style = root_ui()
+            .style_builder()
+            .background(
+                Image::from_file_with_format(
+                    include_bytes!("wasm-pong_button_background.png"),
+                     None
+                    ).unwrap())
+            .text_color(Color::from_rgba(0, 255, 0, 255))
+            .text_color_hovered(Color::from_rgba(0, 255, 0, 255))
+            .text_color_clicked(Color::from_rgba(0, 255, 0, 255))
+            .font_size(20)
+            .build();
 
+        let button_skin = Skin {
+            button_style: button_style,
+            ..root_ui().default_skin()
+        };
+
+        if bot_mode {
+            button_text = "2-Player Mode";
+        } else {
+            button_text = "1-Player Mode";
+        }
+
+        // Mode toggle button
+        root_ui().push_skin(&button_skin);
+        if root_ui().button(vec2(50.0, 10.0), button_text) {
+            bot_mode = !bot_mode;
+            score = 0;
+            bot_score = 0;
+        };
+        root_ui().pop_skin();
+
+        // Draw the score
         let text_player = score.to_string();
         let text_bot = bot_score.to_string();
         let text_dimensions_bot = measure_text(&text_bot, None, 40, 1.0);
@@ -206,4 +263,5 @@ fn check_paddle_col_player(ball_x: f32, ball_y: f32, player_x: f32, player_y: f3
 fn check_paddle_col_bot(ball_x: f32, ball_y: f32, bot_x: f32, bot_y: f32) -> bool {
     bot_x - PADDLE_W < ball_x && bot_y < ball_y && bot_y + PADDLE_H > ball_y
 }
+
 
